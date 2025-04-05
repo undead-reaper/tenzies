9f5fac1a5caee92d050b62fca338ccbec15e7e5b
Dharam Soni <dharamsoni1010@gmail.com>
2025-04-05 17:43:20 +0530

    Added object initialization and updated value generation

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,33 @@
 "use client";
 
 import Die from "@/components/Die";
+import type { DieProps } from "@/components/Die";
 import { Button } from "@/components/ui/button";
-import { DicesIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { DicesIcon, Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [diceValues, setDiceValues] = useState<number[]>([
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  ]);
+  const [diceValues, setDiceValues] = useState<DieProps[]>();
+  const [loading, setLoading] = useState(true);
 
-  function generateNewDice() {
-    const newDice = Array.from({ length: 10 }, () =>
-      Math.ceil(Math.random() * 6)
-    );
-    setDiceValues(newDice);
+  function handleGenerateDice() {
+    const result = Array.from({ length: 10 }, () => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
+    setLoading(false);
+    return result;
   }
 
-  useEffect(() => generateNewDice(), []);
+  useEffect(() => {
+    setDiceValues(handleGenerateDice());
+  }, []);
+
+  function handleRollDice() {
+    setLoading(true);
+    setDiceValues(handleGenerateDice());
+  }
 
   return (
     <main className="flex flex-col items-center justify-center-safe p-5 bg-background h-screen">
@@ -27,12 +37,23 @@ export default function Home() {
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
-        <div className="grid grid-cols-5 gap-[1rem] pb-[2.5rem]">
-          {diceValues.map((value, index) => {
-            return <Die key={index} value={value} />;
-          })}
+        <div
+          className={cn(
+            "gap-[1rem] pb-[2.5rem]",
+            loading
+              ? "flex flex-col items-center justify-center"
+              : "grid grid-cols-5"
+          )}
+        >
+          {loading ? (
+            <Loader2 className="animate-spin text-muted-foreground" />
+          ) : (
+            diceValues!.map((die, index) => {
+              return <Die key={index} value={die.value} isHeld={die.isHeld} />;
+            })
+          )}
         </div>
-        <Button onClick={generateNewDice}>
+        <Button onClick={handleRollDice}>
           <DicesIcon />
           Roll
         </Button>
--- components/Die.tsx
+++ components/Die.tsx
@@ -1,12 +1,19 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-interface DieProps {
+export interface DieProps {
   value: number;
+  isHeld: boolean;
 }
 
-const Die = ({ value }: DieProps) => {
+const Die = ({ value, isHeld }: DieProps) => {
   return (
-    <button className="bg-popover h-[2.25rem] w-[2.25rem] rounded-[0.2rem] font-inter font-bold">
+    <button
+      className={cn(
+        "h-[2.25rem] w-[2.25rem] rounded-[0.2rem] font-inter font-bold",
+        isHeld ? "dark:bg-blue-800 bg-blue-400" : "bg-popover"
+      )}
+    >
       {value}
     </button>
   );
